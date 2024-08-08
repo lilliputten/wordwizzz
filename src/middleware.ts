0458fb442351074d1d1ba8e16abdcc3ed78e5600
lilliputten <lilliputten@gmail.com>
2024-08-08 15:51:30 +0300

    Issue #6: Minor changes.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,11 @@ import { authConfig } from './auth.config';
 
 export default NextAuth(authConfig).auth;
 
+/* // NOTE: This alternative approach causes the same multiply import errors as in `auth.config.ts` when using this imported `auth`.
+ * import { auth } from '@/auth';
+ * export default auth;
+ */
+
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
