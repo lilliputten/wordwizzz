42372014d8d8c276ffa99273f990322bf13144db
lilliputten <lilliputten@gmail.com>
2024-08-09 18:30:43 +0300

    Issue #13: Added build information (app-info. dev/prod, client/server) into `constants/build.ts`.

---
+++ src/core/constants/build.ts
@@ -0,0 +1,25 @@
+import { TAppInfo } from '@/core/types/app/TAppInfo';
+
+import appInfoRaw from '@/app-info.json';
+
+export const appInfo: TAppInfo = appInfoRaw;
+
+export const {
+  currentTimeStr, // "2024.08.09 17:11 +0300"
+  currentTimeTag, // "240809-1711"
+  gitBranch, // "6-oauth-providers"
+  gitCommitHash, // "49b752a"
+  projectName, // "nextjs-auth-app"
+  timestamp, // "2024.08.09 17:11 +0300"
+  timetag, // "240809-1711"
+  version, // "0.0.3"
+} = appInfo;
+
+export const versionHash = 'v.' + version + ' / ' + timestamp;
+
+const nodeEnv = process.env.NODE_ENV;
+
+export const isDev = nodeEnv === 'development';
+export const isProd = !isDev;
+export const isClient = typeof window !== 'undefined';
+export const isServer = !isClient;
---
+++ src/core/types/app/TAppInfo.ts
@@ -0,0 +1,10 @@
+export interface TAppInfo {
+  currentTimeStr: string; // "2024.08.09 17:11 +0300"
+  currentTimeTag: string; // "240809-1711"
+  gitBranch: string; // "6-oauth-providers"
+  gitCommitHash: string; // "49b752a"
+  projectName: string; // "nextjs-auth-app"
+  timestamp: string; // "2024.08.09 17:11 +0300"
+  timetag: string; // "240809-1711"
+  version: string; // "0.0.3"
+}
